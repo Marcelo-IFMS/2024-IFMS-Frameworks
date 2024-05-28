670b28a7aa63eeeee692c6526e7be8146834dc56
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-05-28 14:33:30 -0400

    versão consolidada

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var app = require('./config/server');
+var app = require('./config/server'); // configuração de rota do servidor
 
-var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaHome = require('./app/routes/home')(app);//rota para a página inicial
 
-var rotaHome = require('./app/routes/home')(app);
+var rotaNoticias = require('./app/routes/noticias')(app); // rota para notícias
 
-var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticias')(app);
+var rotaesportes = require('./app/routes/esportes')(app); // Rota para as notícias de esportes
 
-var rotaesportes = require('./app/routes/esportes')(app);
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticias')(app);// Rota para formulário de inclusão
 
-var rotacursos = require('./app/routes/esportes')(app);
+var rotacursos = require('./app/routes/cursos')(app); //
 
-var rotapesquisa = require('./app/routes/esportes')(app);
+var rotapesquisa = require('./app/routes/pesquisa')(app);
 
 //deve ser a última rota
 var rota404 = require('./app/routes/404')(app);
