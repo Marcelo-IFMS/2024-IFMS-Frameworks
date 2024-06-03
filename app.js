3c6628e219e666f722fbfec9b047069b018ffdad
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-06-03 15:45:12 -0400

    limpando app

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 var app = require('./config/server'); // configuração de rota do servidor
 
-//var rotaHome = require('./app/routes/home')(app);//rota para a página inicial
-
-// var rotaNoticias = require('./app/routes/noticias')(app); // rota para notícias
-
-// var rotaesportes = require('./app/routes/esportes')(app); // Rota para as notícias de esportes
-
-// var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticias')(app);// Rota para formulário de inclusão
-
-// var rotacursos = require('./app/routes/cursos')(app); //
-
-// var rotapesquisa = require('./app/routes/pesquisa')(app);
-
-//deve ser a última rota
-//var rota404 = require('./app/routes/404')(app);
-app.use(function(req,res){
+app.use(function (req, res) { //redirecionando 404
     res.status(404).render('secao/404');
 });
+
 app.listen(3000, function () {
     console.log('Servidor ON');
 });
